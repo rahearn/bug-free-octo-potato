4302478daa5be90edfdd36b4d34a2b0a4ea5ac50
Ryan Ahearn <ryan.ahearn@gsa.gov>
2022-07-28 10:24:04 -0400

    Work around zscaler cert in development

--- config/initializers/github.rb
+++ config/initializers/github.rb
@@ -1,3 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, Rails.application.credentials.github[:key], Rails.application.credentials.github[:secret]
+  ssl = if Rails.env.development?
+    {verify: false}
+  end
+  provider :github, Rails.application.credentials.github[:key], Rails.application.credentials.github[:secret], client_options: {ssl: ssl}
 end
